0d2bd8369729bdc9241a91b67444668dcb9aefcc
lautarotetamusa <lautarotetamusa@gmail.com>
2024-03-04 07:44:10 -0300

    Limite para la cntidad de mensajes en el GET

--- src/models/chat.model.ts
+++ src/models/chat.model.ts
@@ -95,6 +95,7 @@ export async function get_messages(chat_id: number): Promise<ChatMessage[]>{
         FROM Messages
         WHERE chat_id = ?
         ORDER BY created_at DESC
+        LIMIT 50
     `;
     const [rows] = await sql.query<RowDataPacket[]>(query, [chat_id]);
     return rows as ChatMessage[];
